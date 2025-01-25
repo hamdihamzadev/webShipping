4b787e6345bd797319ab6f9f20f073c3edb95498
hamdi Git <hamdihamza440@gmail.com>
2025-01-25 14:46:19 +0100

    merge

--- src/components/Home/AboutUs.vue
+++ src/components/Home/AboutUs.vue
@@ -2,15 +2,15 @@
     <section id="aboutUs">
         <b-container class="mb-5">
             <b-row>
-                <b-col cols="12" lg="6" >
+                <b-col cols="12" lg="6">
                     <b-row class="g-0">
-                        <b-col md="4">
+                        <b-col cols="4">
                             <div id="container-title" class="position-relative">
                                 <TitleSection :title-part1="'About'" :title-part2="'Us'" />
                                 <div id="radius-btm-right" class="position-absolute"></div>
                             </div>
                         </b-col>
-                        <b-col md="8">
+                        <b-col cols="8">
                             <div id="item2"></div>
                         </b-col>
                         <b-col cols="12">
@@ -24,14 +24,19 @@
                                     and
                                     innovative technology .
                                 </p>
+
+                                <div class="cercle">
+                                    <span>T</span><span>e</span><span>x</span><span>t</span><span>e</span>
+                                    <span>c</span><span>o</span><span>u</span><span>r</span><span>b</span><span>é</span>
+                                </div>
                             </div>
                         </b-col>
                     </b-row>
                 </b-col>
-                <b-col cols="12" lg="6" >
+                <b-col cols="12" lg="6">
                     <div id="container-img" class="d-flex align-items-center justify-content-end position-relative">
-                        <div class="position-absolute z-1" id="img-topology" >
-                            <img :src="require('@/assets/img/Topology-1.png')"  alt="">
+                        <div class="position-absolute z-1" id="img-topology">
+                            <img :src="require('@/assets/img/Topology-1.png')" alt="">
                         </div>
                         <div class="position-absolute z-1" id="blur">
                             <BlurBg />
@@ -49,7 +54,6 @@
 <script>
     import BlurBg from '../global/BlurBg.vue';
     import TitleSection from '../TitleSection.vue';
-
 
     export default {
         name: 'AboutUs',
@@ -99,23 +103,82 @@
             border-top-left-radius: var(--BRadius-Section);
             border-radius: var(--BRadius-Section) 0 var(--BRadius-Section) var(--BRadius-Section);
             padding: var(--paddingInline-section);
-            p{
+
+            p {
                 text-align: justify;
             }
+
+
+            .cercle span {
+                position: absolute;
+                transform-origin: 100px 100px;
+                /* Centre du cercle */
+                font-size: 18px;
+            }
+
+            .cercle span:nth-child(1) {
+                transform: rotate(0deg);
+            }
+
+            .cercle span:nth-child(2) {
+                transform: rotate(30deg);
+            }
+
+            .cercle span:nth-child(3) {
+                transform: rotate(60deg);
+            }
+
+            .cercle span:nth-child(4) {
+                transform: rotate(90deg);
+            }
+
+            .cercle span:nth-child(5) {
+                transform: rotate(120deg);
+            }
+
+            .cercle span:nth-child(6) {
+                transform: rotate(150deg);
+            }
+
+            .cercle span:nth-child(7) {
+                transform: rotate(180deg);
+            }
+
+            .cercle span:nth-child(8) {
+                transform: rotate(210deg);
+            }
+
+            .cercle span:nth-child(9) {
+                transform: rotate(240deg);
+            }
+
+            .cercle span:nth-child(10) {
+                transform: rotate(270deg);
+            }
+
+            .cercle span:nth-child(11) {
+                transform: rotate(300deg);
+            }
+
+            .cercle span:nth-child(12) {
+                transform: rotate(330deg);
+            }
+
         }
 
-
         #container-img {
+
             background-color: var(--background-section);
             height: 450px;
             border-radius: var(--BRadius-Section);
             padding: var(--paddingInline-section);
+            overflow: hidden;
 
-            #img-topology{
+            #img-topology {
                 top: 73px;
-    width: 182px;
-    left: 100px;
-    opacity: 0.6;
+                width: 182px;
+                left: 100px;
+                opacity: 0.6;
             }
 
             #blur {
